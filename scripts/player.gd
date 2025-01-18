9dbab963dc1e8644721827e597e194e2b441744d
Daniel Trakas <danielgtrakas@gmail.com>
2025-01-18 11:39:48 -0500

    inventory system almost done

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@export var inv: Inv
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,3 +24,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func collect(item):
+	inv.insert(item)
